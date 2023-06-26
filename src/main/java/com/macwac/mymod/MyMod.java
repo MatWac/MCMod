e1daaa5cbcc16b65ce78c7d136b56d743ac82093
Mat <mattwac59@gmail.com>
2023-06-26 19:14:11 +0200

    Start

--- src/main/java/com/macwac/mymod/MyMod.java
+++ src/main/java/com/macwac/mymod/MyMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.macwac.mymod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(MyMod.MOD_ID)
+public class MyMod
 {
+
+    public static final String MOD_ID = "mymod";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public MyMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -47,8 +49,6 @@ public class ExampleMod
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
@@ -59,10 +59,7 @@ public class ExampleMod
 
     private void processIMC(final InterModProcessEvent event)
     {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
+
     }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
